ab633c07ea2f6cfbe956b45f041323527a330346
HaneesH1994 <94160411+HaneesH1994@users.noreply.github.com>
2022-02-16 12:53:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,86 @@
+#call the existing vpc
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.1.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "pubsub"
+  }
+}
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "internetgateway"
+  }
+}
+
+resource "aws_route_table" "routing" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+	}
+  tags = {
+    Name = "my-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.routing.id
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TCP from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+
+
+# create ec2 instance 
+
+resource "aws_instance" "new" {
+  ami           = "ami-0e472ba40eb589f49"
+  instance_type = "t2.micro"
+  vpc_id = aws_vpc.main.id
+  subnet_id = aws_subnet.subnet1.id
+  key_name = "hinux"
+
+  tags = {
+    Name = "terra_insta"
+  }
+}
